41a318dac8e1ca1fe7c5d921a240d5b256a7374b
R M Sivasankar <rmsivasankar@protonmail.com>
2024-12-03 21:24:23 +0530

    Email Verification

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,12 +1,44 @@
 "use client"
 
+import { useCallback, useEffect, useState } from "react";
 import { CardWrapper } from "./card-wrapper";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
 
 export const NewVerificationForm = ()=> {
+
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
+
     const searchParams = useSearchParams();
     const token = searchParams.get("token");
+
+    const onSubmit = useCallback(() => {
+
+        if (success || error) return;
+
+        if (!token) {
+            setError("Missing token!")   
+            return;
+        }
+
+        newVerification(token)
+            .then((data) => {
+                setSuccess(data.success);
+                setError(data.error);
+            })
+            .catch(() => {
+                setError("Something went wrong!");
+            })
+    }, [token, success, error]);
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit]);
+
     return(
         <CardWrapper
             headerLabel="Confirming your verification"
@@ -14,7 +46,13 @@ export const NewVerificationForm = ()=> {
             backButtonHref="/auth/login"
         >
             <div className="flex items-center w-full justify-center">
-                <BeatLoader />
+                {!success && !error && (
+                    <BeatLoader />
+                )}
+                <FormSuccess message={success} />
+                {!success && (
+                    <FormError message={error} />
+                )}
             </div>
         </CardWrapper>
     )
